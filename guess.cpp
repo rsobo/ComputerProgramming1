4bceabe0c56abe67d906dc879239b3c1c165297d
Ryan Sobolewski <ryansobolewski2000@gmail.com>
2022-02-14 11:02:31 -0500

    Update guess.cpp

--- guess.cpp
+++ guess.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-	/**srand(time(0));
+	srand(time(0));
 	int num = rand() % 101;
 	int guess;
 	while(guess != num){
@@ -20,7 +20,7 @@ int main(){
 		else{
 			cout << "You're winner\n";
 		}
-	}*/
+	}
 	for(int i = 1; i < 12; i++){
 		for(int j = 1; j < 12; j++){
 			cout << setw(4) << j * i << " ";
